9ca088777456de74c7bcb111f3beb7d235cb23e9
aacchung <alexandria.chung@g.austincc.edu>
2021-09-07 17:22:45 -0500

    2nd Commit to push files to GitHub

--- src/examples/02_expressions/01_expressions/expressions.h
+++ src/examples/02_expressions/01_expressions/expressions.h
@@ -2,6 +2,13 @@
 Write function prototype operator_precedence_1 with three int parameters
 */
 
+int operator_precedence_1 (int, int, int);
+
 /*
 Write function prototype operator_precedence_2 with three int parameters
 */
+int operator_precedence_2 (int, int, int);
+
+int operator_precedence_3 (int, int, int);
+
+int operator_precedence_4 (int, int, int);
